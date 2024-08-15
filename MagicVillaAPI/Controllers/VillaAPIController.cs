309c0e980bdb41f584a3b2deff63e4168c3dba93
Ederson Silva <edsjcbra@gmail.com>
2024-08-15 09:42:07 +0100

    Configured DbContext

---
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,19 @@
+﻿using MagicVillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaAPI.Controllers;
+
+[Route("api/VillaAPI")]
+[ApiController]
+public class VillaAPIController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Villa> GetVillas()
+    {
+        return new List<Villa>
+        {
+            new Villa { Id = 1, Name = "Pool View" },
+            new Villa { Id = 2, Name = "Beach View" }
+        };
+    }
+}
